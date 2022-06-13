e7d23f1af37b5094d34b4d8086c9b12d2991537f
Nate Owl <104773272+nateowl02@users.noreply.github.com>
2022-06-14 04:21:28 +0800

    Combined Directed and Directional Weapons into one

--- bullet_hell_project/Assets/Scripts/Weapon/WeaponController.cs
+++ bullet_hell_project/Assets/Scripts/Weapon/WeaponController.cs
@@ -11,7 +11,8 @@ public class WeaponController : MonoBehaviour
     public int secondaryTurretCount;
     public float primaryRateOfFire;
     public float secondaryRateOfFire;
-    public float spread;
+    public float startSpread;
+    public float endSpread;
     public float startSpeed;
     public float endSpeed;
     public float offset;
@@ -21,15 +22,7 @@ public class WeaponController : MonoBehaviour
     [Space]
     [Header("Weapon Modes")]
     public bool isInverted = false;
-    public enum WeaponModes
-    {
-        None,
-        Directional,
-        Directed
-
-    };
-
-    public WeaponModes weaponMode;
+    public bool isLocking = false;
 
     Weapon weapon;
     float fireCounter;
@@ -52,76 +45,44 @@ public class WeaponController : MonoBehaviour
 
     IEnumerator ShootingController()
     {
-        if (weaponMode == WeaponModes.Directional)
+        // Mathf.Lerp(startSpread, endSpread, (float)i / secondaryTurretCount)
+        for (int i = 0; i < secondaryTurretCount; i++)
         {
-            for (int i = 0; i < secondaryTurretCount; i++)
+            float temp_angle = weapon.GetStartingAngle(primaryTurretCount, startSpread);
+            float temp_offset = weapon.GetStartingOffset(primaryTurretCount, offset);
+
+            if (isLocking) 
             {
-
-                float temp_angle = weapon.GetStartingAngle(primaryTurretCount, spread);
-                float temp_offset = weapon.GetStartingOffset(primaryTurretCount, offset);
-
-                temp_angle = isInverted ? temp_angle + 180 : temp_angle;
-                temp_offset = isInverted ? temp_offset * -1 : temp_offset;
-
-                for (int j = 0; j < primaryTurretCount; j++)
-                {
-                    weapon.Shoot(new MissileProperties(
-                        direction: new Vector3(Mathf.Cos(temp_angle * Mathf.Deg2Rad), Mathf.Sin(temp_angle * Mathf.Deg2Rad), 0),
-                        position: transform.position + new Vector3(temp_offset, 0, 0),
-                        startSpeed: startSpeed,
-                        endSpeed: endSpeed,
-                        spread: temp_angle + 90
-                        )
-                        
-                    );
-
-                    temp_angle += spread;
-                    temp_offset += isInverted ? offset : -offset;
-
-                }
-
-                yield return new WaitForSeconds(secondaryRateOfFire);
-            }
-
-        }
-
-        if (weaponMode == WeaponModes.Directed)
-        {
-            Vector3 v = Vector3.up;
-            bool isFound = weapon.GetObjectPosition(ref v, targetTag);
-     
-            for (int i = 0; i < secondaryTurretCount; i++)
-            {
-                float temp_angle = weapon.GetStartingAngle(primaryTurretCount, spread);
-                float temp_offset = weapon.GetStartingOffset(primaryTurretCount, offset);
-
+                Vector3 v = Vector3.up;
+                weapon.GetObjectPosition(ref v, targetTag);
                 Vector3 temp_direction = v - transform.position;
                 temp_direction = temp_direction.normalized;
-                temp_angle = temp_angle -Mathf.Atan2(temp_direction.x, temp_direction.y) * Mathf.Rad2Deg;
-
-                temp_angle = isInverted ? temp_angle + 180 : temp_angle;
-                temp_offset = isInverted ? temp_offset * -1 : temp_offset;
-
-                for (int j = 0; j < primaryTurretCount; j++)
-                {
-
-                    weapon.Shoot(new MissileProperties(
-                        direction: new Vector3(Mathf.Cos((temp_angle) * Mathf.Deg2Rad), Mathf.Sin((temp_angle) * Mathf.Deg2Rad), 0),
-                        position: transform.position + new Vector3(temp_offset, 0, 0),
-                        startSpeed: startSpeed,
-                        endSpeed: endSpeed,
-                        spread: temp_angle + 90
-                        )
-
-                    );
-
-                    temp_angle += spread;
-                    temp_offset += isInverted ? offset : -offset;
-                }
-
-                yield return new WaitForSeconds(secondaryRateOfFire);
+                temp_angle = temp_angle - Mathf.Atan2(temp_direction.x, temp_direction.y) * Mathf.Rad2Deg;
             }
+            
+            temp_angle = isInverted ? temp_angle + 180 : temp_angle;
+            temp_offset = isInverted ? temp_offset * -1 : temp_offset;
+
+            for (int j = 0; j < primaryTurretCount; j++)
+            {
+
+                weapon.Shoot(new MissileProperties(
+                    direction: new Vector3(Mathf.Cos(temp_angle * Mathf.Deg2Rad), Mathf.Sin(temp_angle * Mathf.Deg2Rad), 0),
+                    position: transform.position + new Vector3(temp_offset, 0, 0),
+                    startSpeed: startSpeed,
+                    endSpeed: endSpeed,
+                    spread: temp_angle + 90
+                    )
+
+                );
+
+                temp_angle += startSpread;
+                temp_offset += isInverted ? offset : -offset;
+            }
+
+            yield return new WaitForSeconds(secondaryRateOfFire);
         }
+  
 
     }
 }
